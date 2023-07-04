aee7c04ea0d8e6dc938be3482fa3a5499dde7daa
Atahan Çelebi <atahancelebi98@gmail.com>
2023-07-04 14:43:18 +0300

    userInfo method created

--- userProject/userProject/User.cs
+++ userProject/userProject/User.cs
@@ -1,23 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+
 namespace userProject
 {
-	public class User
-	{
-		public int userID { get; set; }
-		public string userName { get; set; }
-		public List<UserPhone> UserPhones { get; set; }
-		public UserEmail UserEmail { get; set; }
-        
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public List<UserPhone> UserPhones { get; set; }
+        public UserEmail UserEmail { get; set; }
 
-
-		public User()
-		{
+        public User()
+        {
             UserPhones = new List<UserPhone>();
             UserEmail = new UserEmail();
+        }
+
+        public void AddPhone(UserPhone phone)
+        {
+            UserPhones.Add(phone);
+        }
+
+        public void RemovePhone(UserPhone phone)
+        {
+            UserPhones.Remove(phone);
+        }
+
+        public void UpdateEmail(UserEmail email)
+        {
+            UserEmail = email;
+        }
+
+		public void UserInfo()
+		{
+			Console.WriteLine("User ID: " + this.UserID);
+            Console.WriteLine("User Name: " + this.UserName);
+            Console.WriteLine("User Email Address: " + this.UserEmail.EmailAddress);
+
+            Console.WriteLine("User Phones:");
+            foreach (var phone in this.UserPhones)
+            {
+                Console.WriteLine("Phone Number: " + phone.PhoneNumber);
+            }
+
 		}
-
     }
-
 }
-
