681d701dbf83af656fce7782de372131f6238980
Atahan Çelebi <atahancelebi98@gmail.com>
2023-07-04 15:01:25 +0300

    crud methods created

--- userProject/userProject/Program.cs
+++ userProject/userProject/Program.cs
@@ -1,42 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace userProject
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            User user = new User();
-            user.userID = 1;
-            user.userName = "Atahan";
-
-            UserPhone phone = new UserPhone();
-            phone.userID = user.userID; //ata
-            phone.UserPhoneId = 1001;
-            phone.PhoneNumber = 5554433;
+            User user = CreateUser("Ata","ata@ata.com");
+            AddUserPhone(user,"90550505");
+            AddUserPhone(user,"30552123");
+            user.UserInfo();
 
 
-            UserPhone phone2 = new UserPhone();
-            phone2.userID = user.userID;//ata
-            phone.UserPhoneId = 1002;
-            phone2.PhoneNumber = 2233412;
+            UpdateUserEmail(user,"atahanc@atahanc.com");
+            RemoveUserPhone(user,"30552123");
+            user.UserInfo();
 
-            user.UserPhones.Add(phone);
-            user.UserPhones.Add(phone2);
+            UpdateUserPhone(user,"30552123","55232123");
+            user.UserInfo();
 
-            //Console.WriteLine($"Kullanıcı {user.userName} ulaşım için {user.UserPhones.ForEach(item=>item.PhoneNumber)}");
-            Console.WriteLine($"Kullanıcı {user.userName} ulaşım için {string.Join(", ", user.UserPhones.Select(item => item.PhoneNumber))}");
+            UpdateUserPhone(user,"90550505","55232123");
+            user.UserInfo();
 
+            //Console.ReadLine();
+        }
 
-            user.UserEmail.UserEmailId = 0;
-            user.UserEmail.UserEmailAdress = "ata@ata.com";
-            user.UserEmail.UserID = user.userID; //userid =1 ata
+        static User CreateUser(string userName,string emailAdress)
+        {
+            Random random = new Random();
+            User user = new User()
+            {
+                UserID = random.Next(1,100),
+                UserName = userName,
+                UserEmail = new UserEmail(){ EmailAddress =emailAdress}
+            };
 
-            Console.WriteLine($"Kullanıcı {user.userName} eposta adresi: {user.UserEmail.UserEmailAdress}");
+            return user;
+        }
 
+        static void UpdateUserEmail(User user,string emailAdress)
+        {
+            Random random = new Random();
+            UserEmail email = new UserEmail()
+            {
+                UserEmailID = random.Next(1,100),
+                EmailAddress = emailAdress,
+                UserID = user.UserID
+            };
 
-            Console.ReadLine();
+            user.UpdateEmail(email);
+        }
+
+        static void AddUserPhone(User user,string phoneNum)
+        {
+            UserPhone phone = new UserPhone()
+            {
+                PhoneID = 1,
+                PhoneNumber = phoneNum,
+                UserID = user.UserID
+            };
+
+            user.AddPhone(phone);
+        }
+
+        static void RemoveUserPhone(User user,string phoneNum)
+        {
+            
+        UserPhone phoneToRemove = user.UserPhones.FirstOrDefault(p => p.PhoneNumber == phoneNum);
+
+        if (phoneToRemove != null)
+        {
+            
+            user.UserPhones.Remove(phoneToRemove);
+        }
+        else
+        {
+            Console.WriteLine($"Telefon ({phoneNum}) bulunamadı.");
+        }
+        }
+
+        static void UpdateUserPhone(User user,string oldPhone, string newPhone)
+        {
+            UserPhone phoneToUpdate = user.UserPhones.FirstOrDefault(p => p.PhoneNumber == oldPhone);
+
+            if(phoneToUpdate != null)
+            {
+                phoneToUpdate.PhoneNumber = newPhone;
+            }
+            else
+            {
+                Console.WriteLine($"Telefon ({oldPhone}) bulunamadı.");
+            }
         }
     }
 }
