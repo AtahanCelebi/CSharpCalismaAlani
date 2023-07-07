d1e83adf7407892117bda2702e01f29754b65d55
Atahan Çelebi <atahancelebi98@gmail.com>
2023-07-07 13:27:21 +0300

    User class CreateUser method implementation

--- userProject/userProject/Program.cs
+++ userProject/userProject/Program.cs
@@ -6,91 +6,15 @@ namespace userProject
     {
         static void Main(string[] args)
         {
-            User user = CreateUser("Ata","ata@ata.com");
-            AddUserPhone(user,"90550505");
-            AddUserPhone(user,"30552123");
-            user.UserInfo();
+      User user1 = new User().CreateUser(1, "John Doe", "johndoe@example.com", "1234567890");
+      UserPhone newphone = new UserPhone{PhoneID=1,PhoneNumber="5555",UserID=user1.UserID};
+      user1.AddPhone(newphone);
+user1.UserInfo();
 
 
-            UpdateUserEmail(user,"atahanc@atahanc.com");
-            RemoveUserPhone(user,"30552123");
-            user.UserInfo();
 
-            UpdateUserPhone(user,"30552123","55232123");
-            user.UserInfo();
-
-            UpdateUserPhone(user,"90550505","55232123");
-            user.UserInfo();
 
             //Console.ReadLine();
-        }
-
-        static User CreateUser(string userName,string emailAdress)
-        {
-            Random random = new Random();
-            User user = new User()
-            {
-                UserID = random.Next(1,100),
-                UserName = userName,
-                UserEmail = new UserEmail(){ EmailAddress =emailAdress}
-            };
-
-            return user;
-        }
-
-        static void UpdateUserEmail(User user,string emailAdress)
-        {
-            Random random = new Random();
-            UserEmail email = new UserEmail()
-            {
-                UserEmailID = random.Next(1,100),
-                EmailAddress = emailAdress,
-                UserID = user.UserID
-            };
-
-            user.UpdateEmail(email);
-        }
-
-        static void AddUserPhone(User user,string phoneNum)
-        {
-            UserPhone phone = new UserPhone()
-            {
-                PhoneID = 1,
-                PhoneNumber = phoneNum,
-                UserID = user.UserID
-            };
-
-            user.AddPhone(phone);
-        }
-
-        static void RemoveUserPhone(User user,string phoneNum)
-        {
-            
-        UserPhone phoneToRemove = user.UserPhones.FirstOrDefault(p => p.PhoneNumber == phoneNum);
-
-        if (phoneToRemove != null)
-        {
-            
-            user.UserPhones.Remove(phoneToRemove);
-        }
-        else
-        {
-            Console.WriteLine($"Telefon ({phoneNum}) bulunamadı.");
-        }
-        }
-
-        static void UpdateUserPhone(User user,string oldPhone, string newPhone)
-        {
-            UserPhone phoneToUpdate = user.UserPhones.FirstOrDefault(p => p.PhoneNumber == oldPhone);
-
-            if(phoneToUpdate != null)
-            {
-                phoneToUpdate.PhoneNumber = newPhone;
-            }
-            else
-            {
-                Console.WriteLine($"Telefon ({oldPhone}) bulunamadı.");
-            }
         }
     }
 }
--- userProject/userProject/User.cs
+++ userProject/userProject/User.cs
@@ -1,48 +1,78 @@
-﻿using System.Collections.Generic;
-
-namespace userProject
+﻿using System.Collections.Generic; 
+using userProject; 
+public class User
 {
-    public class User
+    public int UserID { get; set; }
+    public string UserName { get; set; }
+    public List<UserPhone> UserPhones { get; set; }
+    public UserEmail UserEmail { get; set; }
+
+    public User()
     {
-        public int UserID { get; set; }
-        public string UserName { get; set; }
-        public List<UserPhone> UserPhones { get; set; }
-        public UserEmail UserEmail { get; set; }
-
-        public User()
-        {
-            UserPhones = new List<UserPhone>();
-            UserEmail = new UserEmail();
-        }
-
-        public void AddPhone(UserPhone phone)
-        {
-            UserPhones.Add(phone);
-        }
-
-        public void RemovePhone(UserPhone phone)
-        {
-            UserPhones.Remove(phone);
-        }
-
-        public void UpdateEmail(UserEmail email)
-        {
-            UserEmail = email;
-        }
-
-		public void UserInfo()
-		{
-			Console.WriteLine("---------------------------------");
-			Console.WriteLine("User ID: " + this.UserID);
-            Console.WriteLine("User Name: " + this.UserName);
-            Console.WriteLine("User Email Address: " + this.UserEmail.EmailAddress);
-
-            Console.WriteLine("User Phones:");
-            foreach (var phone in this.UserPhones)
-            {
-                Console.WriteLine("Phone Number: " + phone.PhoneNumber);
-            }
-			Console.WriteLine("---------------------------------");
-		}
+        UserPhones = new List<UserPhone>();
+        UserEmail = new UserEmail();
     }
+
+    public User(int userID, string userName)
+    {
+        UserID = userID;
+        UserName = userName;
+        UserPhones = new List<UserPhone>();
+        UserEmail = new UserEmail();
+    }
+
+    public void AddPhone(UserPhone phone)
+    {
+        UserPhones.Add(phone);
+    }
+
+    public void RemovePhone(UserPhone phone)
+    {
+        UserPhones.Remove(phone);
+    }
+
+    public void UpdateEmail(UserEmail email)
+    {
+        UserEmail = email;
+    }
+
+    public void UserInfo()
+    {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("User ID: " + UserID);
+        Console.WriteLine("User Name: " + UserName);
+        Console.WriteLine("User Email Address: " + UserEmail.EmailAddress);
+
+        Console.WriteLine("User Phones:");
+        foreach (var phone in UserPhones)
+        {
+            Console.WriteLine("Phone Number: " + phone.PhoneNumber);
+        }
+        Console.WriteLine("---------------------------------");
+    }
+    
+    public User CreateUser(int userID, string userName, string emailAddress, string phoneNumber)
+{
+    User user = new User(userID, userName);
+
+    UserPhone phone = new UserPhone()
+    {
+        PhoneID = 1,
+        PhoneNumber = phoneNumber,
+        UserID = user.UserID
+    };
+
+    UserEmail email = new UserEmail()
+    {
+        UserEmailID = 1,
+        EmailAddress = emailAddress,
+        UserID = user.UserID
+    };
+
+    user.AddPhone(phone);
+    user.UpdateEmail(email);
+
+    return user;
+}
+
 }
